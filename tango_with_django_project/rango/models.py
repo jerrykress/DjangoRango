39806de6c579e9cf0b0c8a5e66cdd148b3d7a13f
jerrykress <jerrykress.wsj@gmail.com>
2021-07-29 00:11:49 +0100

    Added Slug to Model

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -5,6 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
 
     class Meta:
         verbose_name_plural = "Categories"
