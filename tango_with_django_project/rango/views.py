38517d57c68dde9173a9feb6ac820d98374e320c
jerrykress <jerrykress.wsj@gmail.com>
2021-07-28 14:37:52 +0100

    Added Templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     html = "Rango says hey there partner! " + "<a href='/rango/about/'>About</a>"
-    return HttpResponse(html)
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, "rango/index.html", context=context_dict)
 
 
 def about(request):
